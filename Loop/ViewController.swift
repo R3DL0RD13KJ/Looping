25c99ab9f65c5e1fd3db6475c577e5eab6a0d041
Kenneth Johnson <kd123johnson@gmail.com>
2021-11-02 10:45:28 -0600

    MVP

--- Loop/ViewController.swift
+++ Loop/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var viewText: UITextView!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func array1(_ sender: Any)
+    {
+        //For Loop
+        var fruit = ["apple", "bananna", "orange", "watermelon"]
+        var output = ""
+        for item in fruit
+        {
+            output += "\(item)\n"
+        }
+        viewText.text = output
+        
+    }
+    
+    @IBAction func array2(_ sender: Any)
+    {
+        var veggies = ["tomato", "carrot", "pea", "celery"]
+        var output = ""
+        for i in 0...3
+        {
+            output += "\(veggies[i])\n"
+        }
+        viewText.text = output
+    }
+    
+    @IBAction func array3(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func array4(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func dictonary(_ sender: Any)
+    {
+        
+    }
+    
+    
+    
 
 }
 
