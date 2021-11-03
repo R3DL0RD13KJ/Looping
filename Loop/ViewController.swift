a905871f9467f63b57e2c1faecd1b7f2757a0af5
Kenneth Johnson <kd123johnson@gmail.com>
2021-11-03 10:40:34 -0600

    Final

--- Loop/ViewController.swift
+++ Loop/ViewController.swift
@@ -45,17 +45,67 @@ class ViewController: UIViewController
     
     @IBAction func array3(_ sender: Any)
     {
-        
+        var drinks = ["tea", "coffee", "milk", "hot chocolate", "soda"]
+        var output = ""
+        for i in 0..<drinks.count
+        {
+        output += "\(drinks[i])\n"
+        }
+        viewText.text = output
     }
+    
     
     @IBAction func array4(_ sender: Any)
     {
+        var snacks = ["chips", "granola", "nuts", "popcorn", "cookies"]
+        var output = ""
+        var x = 0
+        //Do loop will run as long as the end condition is met
+        //The condition can be either at the beggining or end of the loop. Replace the "repeat" code with the condition
+        repeat
+        {
+            output += "\(snacks[x])\n"
+            x += 1
+        }
+        while x < snacks.count
         
+        viewText.text = output
     }
+    
+    
+    @IBAction func array5(_ sender: Any)
+    {
+        var candy = ["hershey kisses", "starburst", "m&ms", "snickers", "skittles", "lolipop", "baby bottle pop", "apple candy", "laffy taffy"]
+        var x = 0
+        var output = ""
+        //Will only enter loop and continue looping while the conditon is true.
+        while x < candy.count
+        {
+            output += "\(candy[x])\n"
+            x += 1
+        }
+        viewText.text = output
+    }
+    
+    @IBAction func array6(_ sender: Any)
+    {
+        var pizza = ["cheese", "pepperoni", "sausage", "peppers", "bacon", "onions", "garlic"]
+        var output = ""
+        
+        output = pizza.joined(separator: "\n")
+        viewText.text = output
+    }
+    
     
     @IBAction func dictonary(_ sender: Any)
     {
-        
+        var contact = ["Name":"Kenneth", "Adress":"169 Minor Street.", "Phone":"156-6969"]
+        var output = ""
+        for (key, value) in contact
+        {
+            output += "\(key): \(value)\n"
+        }
+        viewText.text = output
     }
     
     
